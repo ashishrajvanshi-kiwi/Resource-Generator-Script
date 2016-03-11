a359984eec05f8bf32c6d6e0f620b57d66b2bf2a
Ashish Rajvanshi <ashish.rajvanshi@kiwitech.com>
2016-03-11 16:41:01 +0530

    adding multi platform support in script

--- script.py
+++ script.py
@@ -7,18 +7,25 @@ import cgi
 import os, sys
 
 
-
-available_language = [];
-platform = "android"
-csv_input_file = 'test.csv'
-
-
 clear = lambda: os.system('clear')
 clear()
 
-print "============================  START  ============================"
+
+platforms = sys.argv
+platforms.pop(0)
+
+if not platforms:
+	platforms = ['android', 'ios', 'window']
+
+#			print "\n\n********Invalid argument for platform name  \"", arg , "\" .********"
+#			print "Please selecte only in \"android\",\"ios\" or \"window\".\n\n"
 
 
+available_language = []
+csv_input_file = 'data.csv'
+
+
+# check for available languages to create files
 f = open(csv_input_file)
 csv_f = list(csv.reader(f))
 
@@ -37,45 +44,49 @@ for row in csv_f:
 
 
 
-for language_tab, language in enumerate(available_language):
+print "============================  START  ============================"
 
-	if(language is ""):
-		continue
+for idx, platform in enumerate(platforms):
 
-	outFileName = os.getcwd() + "/" +platform
-	if not os.path.exists(outFileName):
-		os.mkdir(outFileName)
+	for language_tab, language in enumerate(available_language):
 
-	outFileName = platform + "/" + "string_" + language + ".xml"
+		if(language is ""):
+			continue
 
-	print "\n=================", outFileName , "================="
-	with open(outFileName, "wt") as fout:
-	    with open("strings.xml", "rt") as fin:
-	        for line in fin:
+		outFileName = os.getcwd() + "/" + platform
+		if not os.path.exists(outFileName):
+			os.mkdir(outFileName)
 
-				f = open(csv_input_file)
-				csv_f = csv.reader(f)
+		outFileName = platform + "/" + "string_" + language + ".xml"
 
-				replaced = False
-				for row in csv_f:
+		print "\n=================", outFileName , "================="
+		with open(outFileName, "wt") as fout:
+			with open("strings.xml", "rt") as fin:
+				for line in fin:
 
-					str = ' '.join(row)
-					str = cgi.escape(str)
+					f = open(csv_input_file)
+					csv_f = csv.reader(f)
 
-					rfrom = row[0].strip()
-					rto = cgi.escape(row[language_tab+1])
+					replaced = False
+					for row in csv_f:
+
+#						str = ' '.join(row)
+#						str = cgi.escape(str)
+
+						rfrom = row[0].strip()
+						rto = cgi.escape(row[language_tab+1])
 
 
-					if ">" + rfrom + "<" in line: 
+						if ">" + rfrom + "<" in line: 
 
-						line = line.replace(rfrom,rto)
-						line = line.replace('&lt;','<')
-						line = line.replace('&gt;','>')
+							line = line.replace(rfrom,rto)
+							line = line.replace('&lt;','<')
+							line = line.replace('&gt;','>')
+							fout.write(line)
+							print "replacing " , rfrom , " with " , rto
+
+							replaced = True
+					if( not replaced):
 						fout.write(line)
-						print "replacing " , rfrom , " with " , rto
-
-						replaced = True
-				if( not replaced):
-					fout.write(line)
 
 print "============================  FINISH  ============================\n"
